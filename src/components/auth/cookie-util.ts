d5e8c11fa1b700102645fbce8bc17e78305e9d7a
OtzoLive <john@johncwaters.com>
2022-09-19 18:59:15 -0600

    updated cookkie expire to 1 day

--- src/components/auth/cookie-util.ts
+++ src/components/auth/cookie-util.ts
@@ -1,12 +1,11 @@
 /*
  * General utils for managing cookies
  */
-export function setCookie(name: string, val: string, expires: string) {
+export function setCookie(name: string, value: string, expires: string) {
     const date = new Date();
-    const value = val;
 
-    // Set it expire in 7 days
-    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+    // Set it expire in 1 days
+    date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000));
 
     // Set it
     document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";
